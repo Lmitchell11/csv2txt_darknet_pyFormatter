2561ee85a4dcebb4b1084ea64abff1c2ddadcbdc
Liam Mitchell <Lmitchell11@users.noreply.github.com>
2022-04-03 19:35:51 -0400

    Update csv2txt_yoloV4_Darknet_Format.py
    Added in floating-point operations, program should not properly function

--- csv2txt_yoloV4_Darknet_Format.py
+++ csv2txt_yoloV4_Darknet_Format.py
@@ -6,14 +6,20 @@ import os   #imports functions/tools that allow the program to access user/OS fi
 import re   #imports functions/tools that allow the program to read/write files
 import csv  #imports functions/tools that allow the program to read/write .csv files
 
+
+
 i = 0       #initializes j counter for setting previous label in for loop
 k = 0       #initializes k counter for checking if file is open
+darknetArray = []
+
+img_width = 480     #image pixels width size
+img_height = 300    #image pixels height size
 
 
 """Setup your PATH below correspondingly, and insure that this 'python.exe' and 'labels_train.csv' are in a seperately contained folder...
 or you'll be mad for a second"""
 
-f = open("C:/Users/XXXXX/Downloads/Self-Driving Cars_archive/csv2txt_darknet_formatting/labels_train.csv", "r+")    #Comment above
+f = open("C:/Users/runes/Downloads/Self-Driving Cars_archive/csv2txt_darknet_pyFormatter/labels_train.csv", "r+")    #Comment above
 next(f)                                     #skips first line because that is where the HEADER information is
 for line in f:                              #reads line by line
     currentLine = line.split(",")           #splits up line into array, seperates based off commas (CSV format)
@@ -28,7 +34,24 @@ for line in f:                              #reads line by line
             filenameStr = filenameStr + ".txt"                  #changes file extension to .txt
             f2 = open(filenameStr , "w+")                       #opens new file
             k = 1                                               #sets k counter to stop a new file from being opened/written to
-        modifiedCurrLine = str(currentLine[:-1])                #removes class ID data
+
+        darknetArray = []
+        x_center = (int(currentLine[1]) + int(currentLine[2])) / 2
+        y_center = (int(currentLine[3]) + int(currentLine[4])) / 2
+        width = int(currentLine[2]) - int(currentLine[1])
+        height = int(currentLine[4]) - int(currentLine[3])
+        x_center /= img_width 
+        y_center /= img_height 
+        width    /= img_width 
+        height   /= img_height 
+        darknetArray.append("{:.3f} {:.3f} {:.3f} {:.3f}".format(x_center, y_center, width, height))
+
+        
+        modifiedCurrLineLast = currentLine[5].rstrip()
+        modifiedCurrLineLast = int(modifiedCurrLineLast)
+        modifiedCurrLineLast = modifiedCurrLineLast - 1
+        modifiedCurrLine = str(currentLine[:-1])
+        modifiedCurrLine = (str(modifiedCurrLineLast) + " " + str(darknetArray))
         modifiedCurrLine = re.sub(",", "" ,modifiedCurrLine)    #removes comma from csv manipulation
         modifiedCurrLine = re.sub("'", "" ,modifiedCurrLine)    #removes comma from csv manipulation
         modifiedCurrLine = modifiedCurrLine.replace("[", "")    #removes bracket from csv manipulation
